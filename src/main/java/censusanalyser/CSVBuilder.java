0a8bf6ed96628d0d9c6cafc25652242b4b2e44bf
sagar-528 <gupta.sagar528@gmail.com>
2020-04-10 21:25:57 +0530

    REFACTOR: Changed the method name to getCSVMap

--- src/main/java/censusanalyser/CSVBuilder.java
+++ src/main/java/censusanalyser/CSVBuilder.java
@@ -4,18 +4,25 @@ import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 
 import java.io.Reader;
+import java.util.HashMap;
 import java.util.List;
 
 public class CSVBuilder implements IcsvBuilder{
 
     @Override
-    public <T> List<T> getCSVFileList(Reader reader, Class csvClass) throws CSVBuilderException {
+    public <T> HashMap<T> getCSVFileMap(Reader reader, Class csvClass) throws CSVBuilderException {
         try{
                 CsvToBeanBuilder<T> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
                 csvToBeanBuilder.withType(csvClass);
                 csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
                 CsvToBean<T> csvToBean = csvToBeanBuilder.build();
-                return csvToBean.parse();
+                List list = csvToBean.parse();
+                HashMap<Integer, Object> map = new HashMap<>();
+                for (Object record.list){
+                    map.put(count, record);
+                    count++;
+            }
+                return (HashMap<T, T>) map;
             }catch (IllegalStateException e){
                     throw new CSVBuilderException(e.getMessage(),
                         CSVBuilderException.ExceptionType.UNABLE_TO_PARSE);

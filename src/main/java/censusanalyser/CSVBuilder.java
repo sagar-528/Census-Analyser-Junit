6207e6e1df6fe0c09c6e27b4961c2edf4e21843c
sagar-528 <gupta.sagar528@gmail.com>
2020-04-10 23:10:57 +0530

    UPDATE: Removed all the errors

--- src/main/java/censusanalyser/CSVBuilder.java
+++ src/main/java/censusanalyser/CSVBuilder.java
@@ -10,22 +10,23 @@ import java.util.List;
 public class CSVBuilder implements IcsvBuilder{
 
     @Override
-    public <T> List<T> getCSVFileList(Reader reader, Class csvClass) throws CSVBuilderException {
+    public <T> HashMap<T, T> getCSVFileMap(Reader reader, Class csvClass) throws CSVBuilderException {
         try{
-                CsvToBeanBuilder<T> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
-                csvToBeanBuilder.withType(csvClass);
-                csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
-                CsvToBean<T> csvToBean = csvToBeanBuilder.build();
-                List list = csvToBean.parse();
-                HashMap<Integer, Object> map = new HashMap<>();
-                for (Object record.list){
+            CsvToBeanBuilder<T> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
+            csvToBeanBuilder.withType(csvClass);
+            csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+            CsvToBean<T> csvToBean = csvToBeanBuilder.build();
+            List list = csvToBean.parse();
+            HashMap<Integer, Object> map = new HashMap<>();
+            for (Object record : list){
+                Integer count = 0;
                 map.put(count, record);
                 count++;
             }
-                return (HashMap<T, T>) map;
-            }catch (IllegalStateException e){
-                    throw new CSVBuilderException(e.getMessage(),
-                        CSVBuilderException.ExceptionType.UNABLE_TO_PARSE);
-                }
+            return (HashMap<T, T>) map;
+        }catch (IllegalStateException e){
+            throw new CSVBuilderException(e.getMessage(),
+                    CSVBuilderException.ExceptionType.UNABLE_TO_PARSE);
+        }
     }
 }
--- src/main/java/censusanalyser/IcsvBuilder.java
+++ src/main/java/censusanalyser/IcsvBuilder.java
@@ -5,6 +5,5 @@ import java.util.HashMap;
 import java.util.List;
 
 public interface IcsvBuilder {
-   public  <T> List<T> getCSVFileList(Reader reader, Class csvClass) throws CSVBuilderException;
    public  <T> HashMap<T, T> getCSVFileMap(Reader reader, Class csvClass) throws CSVBuilderException;
 }
